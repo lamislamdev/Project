50baddc1778e2207554f3d9576a456d611205cca
Lam Islam <lamislam4444@gmail.com>
2024-05-26 20:24:20 +0600

    Ghost Auth Tool
    Ghost Auth Tool. Xiaomi authorize flash tool

---
+++ Ghostauthtool/script.js
@@ -0,0 +1,8 @@
+let login = document.getElementById('login');
+const loginpage = function() {
+  login.classList.remove('hidden');
+};
+// const login = document.getElementById('login');
+const ext = function() {
+  login.classList.remove('hidden');
+};
